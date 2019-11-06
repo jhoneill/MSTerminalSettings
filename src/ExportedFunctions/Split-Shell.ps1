a621c29e929949b4ecb733d17bfd44b7384f085d
jhoneill <jhoneill@waitrose.com>
2019-11-06 15:56:10 +0000

    Revised split shell

--- src/ExportedFunctions/Split-Shell.ps1
+++ src/ExportedFunctions/Split-Shell.ps1
@@ -24,16 +24,37 @@ function Split-Shell {
             Write-Warning "You need to define a key for the splitHorizontal and/or splitVertical command(s) in settings."
             return
         }
-        foreach ($s in $Name) {
+        foreach ($shell in $Name) {
             #figure out which shell the user meant, make it the default, send the split horizontal or split vertical key, allow things to catch up
-            $p = Resolve-MSTerminalProfile  $S
-            Set-MSTerminalProfile -MakeDefault -name $p.name
-            Start-Sleep -Seconds 1
-            $wshShell.SendKeys($SplitKey)
+                        
+            $TermProfile = switch -regex ($shell) {
+                #GUID
+                "[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}" {Get-MSTerminalProfile -Guid $shell; break}
+                #Single Digit. Return the shell opened by pressing ctrl-shift-Digit. This may be the nth shell in the profile or may be customized
+                #If the user has changed to (e.g.) Ctrl-alt-digit we still match.
+                #If we have ctrl-alt-7 and ctrl-shift-7 both mapped to newTabProfileX we retrun the first one
+                "^\d$"  {
+                    $index = (Get-MSTerminalSetting).keybindings.where({$_.command -like "newTabProfile*" -and $_.keys -like "*$shell"},'First',1)
+                    if (-not $index) {Write-Warning "There isn't an new tab profile command for ctrl-shift-$shell";return}
+                    else  {
+                        $i = $index.command -replace '^.*?(\d+)$','$1'
+                        Write-Verbose "$($Index.keys) maps to $($index.command) - getting profile at postion $i "
+                        Get-MSTerminalProfile | Select-Object -Index $i
+                        Break 
+                    }
+                }
+                #For anything else, assume a string which matches a name. Return only one match.
+                Default {(Get-MSTerminalProfile).Where({$_.Name -Like "*$Shell*"},'First',1)}
+            }    
+            if ($TermProfile) {
+                set-MSTerminalSetting -DefaultProfile $TermProfile.guid-ErrorAction Stop
+                Start-Sleep -Seconds 1
+                $wshShell.SendKeys($SplitKey,$false) 
+            }
         }
     }
     end {
         #Restore the original default profile
-        Get-MSTerminalProfile -Guid $defaultProfile| Set-MSTerminalProfile -MakeDefault
+        set-MSTerminalSetting -DefaultProfile  $defaultProfile 
     }
 }
